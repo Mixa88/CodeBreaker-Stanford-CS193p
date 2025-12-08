e1c2396ef3197dc99775ae0a42b42cb40481b84a
Михаил Тихонов <mishkatix88@gmail.com>
2025-12-08 16:45:26 +0200

    feat: implement MatchMarkers logic and Match enum

---
+++ CodeBreaker/MatchMarkers.swift
@@ -0,0 +1,50 @@
+//
+//  MatchMarkers.swift
+//  CodeBreaker
+//
+//  Created by Михайло Тихонов on 07.12.2025.
+//
+
+import SwiftUI
+
+
+enum Match {
+    case nomatch
+    case exact
+    case inexact
+}
+
+struct MatchMarkers: View {
+    var matches: [Match]
+    
+    var body: some View {
+        HStack {
+            VStack {
+                matchMarker(peg: 0)
+                matchMarker(peg: 1)
+            }
+            
+            VStack {
+                matchMarker(peg: 2)
+                matchMarker(peg: 3)
+            }
+        }
+    }
+    
+    func matchMarker(peg: Int) -> some View {
+        
+        let exactCount: Int = matches.count(where: { $0 == .exact })
+        let foundCount = matches.count(where: { $0 != .nomatch })
+        
+        
+        
+        return Circle()
+            .fill(exactCount > peg ? Color.primary : Color.clear)
+            .strokeBorder(foundCount > peg ? Color.primary : Color.clear, lineWidth: 2)
+            .aspectRatio(1, contentMode: .fit)
+    }
+}
+
+#Preview {
+    MatchMarkers(matches: [.exact, .inexact, .inexact, .exact])
+}
