86c5f8cb809c96f1f238039b49fd66d37e2bec5a
Михаил Тихонов <mishkatix88@gmail.com>
2025-12-12 17:15:25 +0200

    refactor: rewrite matching logic using functional programming (map/reversed) and update Code model

--- CodeBreaker/CodeBreakerView.swift
+++ CodeBreaker/CodeBreakerView.swift
@@ -47,7 +47,7 @@ struct CodeBreakerView: View {
             ForEach(code.pegs.indices, id: \.self) { index in
                 RoundedRectangle(cornerRadius: 10)
                     .overlay {
-                        if code.pegs[index] == Code.missing {
+                        if code.pegs[index] == Code.missingPeg {
                             RoundedRectangle(cornerRadius: 10)
                                 .strokeBorder(Color.gray)
                         }
@@ -62,14 +62,19 @@ struct CodeBreakerView: View {
                     }
             }
             
-            MatchMarkers(matches: code.matches)
+            Rectangle().foregroundStyle(Color.clear).aspectRatio(1, contentMode: .fit)
                 .overlay {
-                    if code.kind == .guess {
-                        guessButton
+                    if let matches = code.matches {
+                        MatchMarkers(matches: matches)
+                    } else {
+                        if code.kind == .guess {
+                            guessButton
+                        }
+                        
                     }
                 }
-        }
     }
+  }
 }
 
 
